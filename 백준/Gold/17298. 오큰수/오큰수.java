0c68f2f058227845ef92a7c6fb6683b2712fc601
taeyeongKims <137798168+taeyeongKims@users.noreply.github.com>
2025-01-10 02:13:48 +0900

    [Gold IV] Title: 오큰수, Time: 984 ms, Memory: 150300 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+        int[] answer = new int[n];
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && arr[stack.peek()] < arr[i]) {
+                answer[stack.pop()] = arr[i];
+            }
+            stack.push(i);
+        }
+
+        while (!stack.isEmpty()) {
+            answer[stack.pop()] = -1;
+        }
+        for(int i = 0; i < n; i++) {
+            bw.write(answer[i] + " ");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
