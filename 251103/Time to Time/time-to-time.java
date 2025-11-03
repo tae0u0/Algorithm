c50a45f9e88d9f6e24a9c24e77bbd15b0549870a
김태영 <137798168+tae0u0@users.noreply.github.com>
2025-11-03 23:00:30 +0900

    Title: Time to Time, Time: 129ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251103/Time to Time/time-to-time.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        int rst = (60 * c + d) - (60 * a + b);
+        System.out.print(rst);
+    }
+}
