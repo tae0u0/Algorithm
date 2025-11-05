a8c07a78bce6d94fdf99d12b317c4eca0c25842e
김태영 <137798168+tae0u0@users.noreply.github.com>
2025-11-05 16:15:01 +0900

    Title: 그 요일은, Time: 130ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 251105/그 요일은/the-day-of-the-day.java
+++ 251105/그 요일은/the-day-of-the-day.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
-    static int[] num_of_days = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    static int[] num_of_days = new int[]{0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int m1 = sc.nextInt();
