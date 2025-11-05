f545ecb4bbc4531b205024e9efe9e81fb051d9e1
김태영 <137798168+tae0u0@users.noreply.github.com>
2025-11-05 17:44:16 +0900

    Title: 진수 to 진수, Time: 123ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 251105/진수 to 진수/transformation-of-number-system.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        String N = sc.next();
+        // Please write your code here.
+        char[] digits = N.toCharArray();
+        int num = 0;
+
+        for(int i = 0; i<digits.length; i++) {
+            num = num * A + Character.getNumericValue(digits[i]);
+        }
+
+        int[] arr = new int[10];
+        int cnt = 0;
+        while(num > B-1){
+            arr[cnt++] = num % B;
+            num /= B;
+        }
+        arr[cnt] = num;
+
+        for(; cnt >= 0; cnt--){
+            System.out.print(arr[cnt]);
+        }
+    }
+}
