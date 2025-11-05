5f7941b793dc7b914edc1d87bf3e5fd2edfa4482
김태영 <137798168+tae0u0@users.noreply.github.com>
2025-11-05 16:14:42 +0900

    Title: 그 요일은, Time: 130ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251105/그 요일은/the-day-of-the-day.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+    static int[] num_of_days = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m1 = sc.nextInt();
+        int d1 = sc.nextInt();
+        int m2 = sc.nextInt();
+        int d2 = sc.nextInt();
+        String A = sc.next();
+        
+        int first = d1, second = d2;
+
+        for(int i = 1; i<m1; i++) first += num_of_days[i];
+        for(int i = 1; i<m2; i++) second += num_of_days[i];
+
+        int dist = second - first + 1;
+        int dist_day_num = getDay(A);
+        int ans = 0;
+        if(dist % 7 >= dist_day_num) ans++;
+        System.out.print(dist / 7 + ans);
+    }
+
+    private static int getDay(String A) {
+        if(A.equals("Mon")) return 0;
+        else if(A.equals("Tue")) return 1;
+        else if(A.equals("Wen")) return 2;
+        else if(A.equals("Thu")) return 3;
+        else if(A.equals("Fri")) return 4;
+        else if(A.equals("Sat")) return 5;
+        
+        return 6;
+    }
+}
