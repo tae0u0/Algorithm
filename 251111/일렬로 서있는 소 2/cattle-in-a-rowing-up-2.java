58628d1c72fee58ca34568aef38b44a0ca275bbf
김태영 <137798168+tae0u0@users.noreply.github.com>
2025-11-11 17:39:42 +0900

    Title: 일렬로 서있는 소 2, Time: 134ms, Memory: 10MB, Status: Passed - Codetree

--- 251111/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.java
+++ 251111/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.java
@@ -12,7 +12,7 @@ public class Main {
         for (int i = 0; i < n - 2; i++) {
             for (int j = i + 1; j < n - 1; j++) {
                 for (int k = j + 1; k < n; k++) {
-                    if(arr[i] < arr[j] && arr[j] < arr[k]) cnt++;
+                    if(arr[i] <= arr[j] && arr[j] <= arr[k]) cnt++;
                 }
             }
         }
