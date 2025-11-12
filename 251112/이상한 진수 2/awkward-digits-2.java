04ea94e15d4b6c196846338a1f651ff7b39ebcb8
김태영 <137798168+tae0u0@users.noreply.github.com>
2025-11-12 11:16:50 +0900

    Title: 이상한 진수 2, Time: 128ms, Memory: 10MB, Status: Passed - Codetree

--- 251112/이상한 진수 2/awkward-digits-2.java
+++ 251112/이상한 진수 2/awkward-digits-2.java
@@ -3,12 +3,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         char[] a = sc.next().toCharArray();
+        boolean flag = false;
         for(int i = 0; i<a.length; i++) {
             if(a[i] == '0') {
                 a[i] = '1';
+                flag = true;
                 break;
             }
         }
+
+        if(!flag) a[a.length - 1] = '0';
 
         int index = 1;
         int cnt = 0;
