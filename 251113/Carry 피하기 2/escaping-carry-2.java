c9d84ed79b78b382d77d1be4e62f9b46e90f1871
김태영 <137798168+tae0u0@users.noreply.github.com>
2025-11-13 23:29:17 +0900

    Title: Carry 피하기 2, Time: 127ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251113/Carry 피하기 2/escaping-carry-2.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        int rst = Integer.MIN_VALUE;
+        for (int i = 0; i< n-2; i++) {
+            for(int j= i+1; j < n-1; j++) {
+                for(int k = j+1; k < n; k++) {
+                    int maxNum = Math.max(arr[i], arr[j]);
+                    maxNum = Math.max(maxNum, arr[k]);
+                    int divisor = 1;
+                    boolean flag = true;
+                    int first = arr[i];
+                    int second = arr[j];
+                    int third = arr[k];
+                    while(maxNum / divisor > 0) {
+                        if(first % 10  + second % 10 + third % 10 >= 10) {
+                            flag = false;
+                            break;
+                        }
+                        first /= 10;
+                        second /= 10;
+                        third /= 10;
+                        divisor *= 10;
+                    }
+                    if(flag) rst = Math.max(rst, arr[i] + arr[j] + arr[k]);
+                }
+            }
+        }
+        System.out.print(rst);
+
+
+    }
+}
