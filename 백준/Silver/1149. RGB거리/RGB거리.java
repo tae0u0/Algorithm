aee0aaf1e4097c824662ffbcb69dcae989119ec0
김태영 <137798168+tae0u0@users.noreply.github.com>
2025-11-18 20:22:19 +0900

    [Silver I] Title: RGB거리, Time: 132 ms, Memory: 15696 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,43 +1,40 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
+    static int N;
+    static int[][] cost;
+    static int[][] dp;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int num = Integer.parseInt(br.readLine().trim());
-        int[][] colorPrice = new int[num + 1][3];
-        for (int i = 1; i <= num; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            colorPrice[i][0] = Integer.parseInt(st.nextToken());
-            colorPrice[i][1] = Integer.parseInt(st.nextToken());
-            colorPrice[i][2] = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(br.readLine());
+        cost = new int[N][3];
+        dp = new int[N][3];
+        for(int i = 0; i < N; i++) {
+            cost[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         }
 
-        int[][] dp = new int[num+1][3];
-        int rst = Integer.MAX_VALUE;
+        dp[0][0] = cost[0][0];
+        dp[0][1] = cost[0][1];
+        dp[0][2] = cost[0][2];
 
-        for(int i=0; i<3; i++){
-            for(int k=0; k<3; k++){
-                if(i==k)
-                    dp[1][i] = colorPrice[1][i];
-                else
-                    dp[1][k] = 100000;
-            }
-            for(int k=2; k<=num; k++){
-                dp[k][0] = Math.min(dp[k-1][1], dp[k-1][2]) + colorPrice[k][0];
-                dp[k][1] = Math.min(dp[k-1][0], dp[k-1][2]) + colorPrice[k][1];
-                dp[k][2] = Math.min(dp[k-1][0], dp[k-1][1]) + colorPrice[k][2];
-            }
-
-            for(int k = 0; k<3; k++){
-                rst = Math.min(dp[num][k], rst);
+        solve();
+        int minNum = Integer.MAX_VALUE;
+        for(int i = 0; i < 3; i++) {
+            if(minNum > dp[N-1][i]) {
+                minNum = dp[N-1][i];
             }
         }
-
-        bw.write(rst + "");
-        bw.flush();
-        bw.close();
+        System.out.println(minNum);
     }
 
+    private static void solve() {
+        for(int i = 1; i < N; i++) {
+            for(int j = 0; j < 3; j++) {
+                int first = cost[i][j] + dp[i-1][(j+1) % 3];
+                int second = cost[i][j] + dp[i-1][(j+2) % 3];
+                dp[i][j] = Math.min(first, second);
+            }
+        }
+    }
 }
