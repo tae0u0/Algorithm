972b87a6231c21e05b8521aed51ce286ad5b8e4c
김태영 <137798168+tae0u0@users.noreply.github.com>
2026-03-13 17:32:35 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 200 ms, Memory: 17884 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N-i; j++) {
+                sb.append(" ");
+            }
+            for (int j = N - i + 1; j <= N; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        System.out.printf(sb.toString());
+    }
+}
