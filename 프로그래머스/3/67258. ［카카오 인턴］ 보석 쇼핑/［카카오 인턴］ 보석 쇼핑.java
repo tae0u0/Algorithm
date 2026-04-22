69640bd54a3584378c25895a19173031ee102ff7
김태영 <137798168+tae0u0@users.noreply.github.com>
2026-04-22 10:21:09 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 76.15 ms, Memory: 83.6 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
@@ -11,26 +11,25 @@ class Solution {
         int[] answer = new int[2];
 
         for (int right = 0; right < gems.length; right++) {
-            // 오른쪽 확장: 현재 보석 추가
             window.merge(gems[right], 1, Integer::sum);
-
-            // 모든 종류가 포함됐을 때, 왼쪽을 최대한 줄임
-            while (window.size() == totalTypes) {
-                int windowLen = right - left + 1;
+            
+            if(window.size() == totalTypes) {
+                while (window.size() == totalTypes) {
+                    String leftGem = gems[left];
+                    if (window.get(leftGem) == 1) {
+                        window.remove(leftGem);
+                    } else {
+                        window.merge(leftGem, -1, Integer::sum);
+                    }
+                    left++;
+                }
+                
+                int windowLen = right - left + 2;
                 if (windowLen < minLen) {
                     minLen = windowLen;
-                    answer[0] = left + 1; // 1-indexed
+                    answer[0] = left;
                     answer[1] = right + 1;
                 }
-
-                // 왼쪽 포인터 이동
-                String leftGem = gems[left];
-                if (window.get(leftGem) == 1) {
-                    window.remove(leftGem); // 제거 시 종류 감소
-                } else {
-                    window.merge(leftGem, -1, Integer::sum);
-                }
-                left++;
             }
         }
 
