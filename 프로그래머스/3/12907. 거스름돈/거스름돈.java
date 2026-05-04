00e862eac536b5704630c25a21717e1da08005e9
김태영 <137798168+tae0u0@users.noreply.github.com>
2026-05-04 17:57:50 +0900

    [level 3] Title: 거스름돈, Time: 19.07 ms, Memory: 54.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int[] money) {
+        int MOD = 1_000_000_007;
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+
+        for (int coin : money) {
+            for (int i = coin; i <= n; i++) {
+                dp[i] = (dp[i] + dp[i - coin]) % MOD;
+            }
+        }
+
+        return dp[n];
+    }
+}
